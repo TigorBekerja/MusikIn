a11a8cc41b4cb10a5122a6aba53b8fc950155963
TigorBekerja <120919621+TigorBekerja@users.noreply.github.com>
2025-08-07 17:08:27 +0700

    update user
    add hashing in password

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, beforeSave, column } from '@adonisjs/lucid/orm'
+import Hash from '@adonisjs/core/services/hash'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
@@ -11,11 +12,18 @@ export default class User extends BaseModel {
   @column()
   declare email : string
 
-  @column()
+  @column({serializeAs: 'password'})
   declare password : string
 
   @column() 
   declare favorite_artist_id? : number 
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
