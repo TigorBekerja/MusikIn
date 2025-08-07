dc13b5e7f5cc8b8c2016aed093dbaacea8154efb
TigorBekerja <120919621+TigorBekerja@users.noreply.github.com>
2025-08-07 11:20:51 +0700

    add user model

---
+++ app/models/user.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare usename : string
+
+  @column()
+  declare email : string
+
+  @column()
+  declare password : string
+
+  @column() 
+  declare favorite_artist_id? : number 
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
